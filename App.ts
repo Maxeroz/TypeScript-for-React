48618b5b00b0fd86f244ca0f2c17e530803c5ba9
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-06-12 03:38:17 +0400

    TypeScript Functions

--- App.ts
+++ App.ts
@@ -83,3 +83,33 @@ testAny = {
 
 let testAnyArray: any[];
 testAnyArray = [1, "two", { nubmer: "three" }];
+
+// FUNCTIONS
+
+let sayHi = () => {
+  console.log("Hi, welcome");
+};
+
+let funcReturnString = (): string => {
+  console.log("hi");
+  return "lama dev";
+};
+
+let multiple = (num: number) => {
+  return num * 2;
+};
+
+let multiple2 = (num: number): number => {
+  return num * 2;
+};
+
+let multiple3 = (num: number): void => {
+  //   return num * 2;
+  //   Do sth but don't return
+};
+
+let sum = (num1: number, num2: number, another?: number) => {
+  return num1 + num2;
+};
+
+sum(2, 3);
